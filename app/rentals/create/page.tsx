a3586d3950b2101e12ce7fa4dec893e3a6224404
Paul Graffam <paulmg@users.noreply.github.com>
2024-12-02 21:12:44 -0500

    Finished property create

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,8 @@ import CategoriesInput from '@/components/form/CategoriesInput';
 import TextAreaInput from '@/components/form/TextAreaInput';
 import CountriesInput from '@/components/form/CountriesInput';
 import ImageInput from '@/components/form/ImageInput';
+import CounterInput from '@/components/form/CounterInput';
+import AmenitiesInput from '@/components/form/AmenitiesInput';
 
 function CreatePropertyPage() {
   return (
@@ -38,6 +40,16 @@ function CreatePropertyPage() {
             <CountriesInput />
             <ImageInput />
           </div>
+
+          <h3 className='text-lg mt-8 mb-4 font-medium'>Accommodation Details</h3>
+          <CounterInput detail='guests' />
+          <CounterInput detail='bedrooms' />
+          <CounterInput detail='beds' />
+          <CounterInput detail='baths' />
+
+          <h3 className='text-lg mt-10 mb-6 font-medium'>Amenities</h3>
+          <AmenitiesInput />
+
           <SubmitButton text='create rental' className='mt-12' />
         </FormContainer>
       </div>
--- utils/actions.ts
+++ utils/actions.ts
@@ -129,15 +129,53 @@ export const updateProfileImageAction = async (prevState: any, formData: FormDat
 }
 
 export const createPropertyAction = async (prevState: any, formData: FormData) : Promise<{message: string}> => {
-  const user = await currentUser();
+  const user = await getAuthUser();
 
   try {
     const rawData = Object.fromEntries(formData);
-    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    const file = formData.get('image') as File;
 
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file});
+    const fullPath = await uploadImage(validatedFile.image);
+
+    await db.property.create({
+      data: {
+        ...validatedFields,
+        image: fullPath,
+        profileId: user.id,
+      }
+    })
   } catch (error) {
     return renderError(error);
   }
 
   redirect('/');
 }
+
+export const fetchProperties = async ({
+                                        search = '',
+                                        category,
+                                      }: {
+  search?: string;
+  category?: string;
+}) => {
+  const properties = await db.property.findMany({
+    where: {
+      category,
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { tagline: { contains: search, mode: 'insensitive' } },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      tagline: true,
+      country: true,
+      image: true,
+      price: true,
+    },
+  });
+  return properties;
+};
