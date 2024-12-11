665526f562ef11b971589a20abac2de8d79f4608
Paul Graffam <paulmg@users.noreply.github.com>
2024-12-11 18:26:28 -0500

    Search functionality

--- components/card/LoadingCards.tsx
+++ components/card/LoadingCards.tsx
@@ -1,9 +1,14 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function LoadingCards() {
-  return <div>
-
-  </div>;
+  return (
+    <section className='mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4'>
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+    </section>
+  );
 }
 
 export function SkeletonCard() {
